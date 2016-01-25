e71838a92284520e85e4dabf333af3d4b8580ee9
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-24 20:27:04 -0500

    accordion JS and basic styling finished

--- app/assets/javascripts/description_box.js
+++ app/assets/javascripts/description_box.js
@@ -1,6 +1,14 @@
 $(function() {
-  $(".att-name").click(function() {
-    $(this).parent().parent().children().children().not(".att-name").hide();
-    $(this).siblings().toggle();
+  $(".attribute").click(function() {
+    $(this).parent().parent().parent().find(".transit-desc").hide();
+    $(this).parent().parent().parent().find(".description").hide();
+    $(this).parent().parent().parent().find(".description").not(".transit-desc").toggle();
+  });
+});
+
+$(function() {
+  $(".transit").click(function() {
+    $(this).parent().find(".description").hide();
+    $(this).parent().find(".transit-desc").toggle();
   });
 });
