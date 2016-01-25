5ee8a4c910f0a51f84ec423eac36cc587dfc5eb3
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-25 18:36:18 -0500

    ip changed for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,10 +34,10 @@ class User < ActiveRecord::Base
 
   def set_location(ip)
     #real code to be implemented in production
-    #response = HTTPClient.new.get("http://freegeoip.net/json/#{ip}")
+    response = HTTPClient.new.get("http://freegeoip.net/json/#{ip}")
     #test code because localhost ip doesn't mesh with api
     #use geocoder gem instead of freegeoip, might solve issue in development.
-    response = HTTPClient.new.get("http://freegeoip.net/json/50.241.127.209")
+    # response = HTTPClient.new.get("http://freegeoip.net/json/50.241.127.209")
     json_response = JSON.parse(response.body)
 
     self.latitude = json_response["latitude"]
