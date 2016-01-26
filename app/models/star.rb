59e40c4d108d3ba6cdcb91d77629710e88dfce61
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-25 19:20:57 -0500

    some descriptions for star attributes written

--- app/models/star.rb
+++ app/models/star.rb
@@ -16,12 +16,44 @@ class Star < ActiveRecord::Base
   ]
 
   ATTRIBUTE_INFO = {
-    starname: "star name info",
-    rightascension: "RA info",
-    declination: "dec info",
-    distance: "dist info",
-    temperature: "temp info",
-    stellarage: "age info",
+    starname: "Many stars have multiple names, but the name represented
+              here is the name referenced most often in publications.  New stars
+              are generally named after either their discoverer, or the
+              telescope with which they were found.  Other stars have been
+              named for centuries, and follow the trends of the society that
+              named them.  This resulted in a fair portion of stars possessing
+              names from the mythologies of various cultures.",
+    rightascension: "Right ascension is the name for one of the coordinates in the
+                    celestial coordinate system.  It is akin to the longitude
+                    of earth's coordinates, in that it runs perpendicular to the
+                    celestial equator.  Declination can be measured in units of
+                    time (hours, minutes, seconds), or in degrees.",
+    declination: "Declination is the name for one of the coordinates in the
+                  celestial coordinate system.  It is akin to the latitude
+                  of earth's coordinates, in that it runs parallel to the
+                  celestial equator.  Declination is measured in degrees with a
+                  value of zero at the celestial equator, positive ninety degrees
+                  at the celestial north pole, and negative ninety degrees at the
+                  celestial south pole",
+    distance: "Distance represents how far the planetary system that hosts this
+              star is from earth.  This distance is measured in parsecs, where a
+              parsec is roughly equivalent to 30,860,000,000,000 kilometers, or
+              3.25 light years.  The distance between the Earth and the Sun is
+              about 0.000004848 parsecs, so visitng this system would be quite
+              the trip!",
+    temperature: "This is the absolute surface temperature of the star, measured
+                  in Kelvin.  Kelvin scales identically to degrees celsius, so at
+                  high tempertures one is a fair estimate for the other.  The
+                  surface temperature of a star is calculated by studying the
+                  light spectra that the star emits, and finding the wavelength
+                  of light that the star emits most frequently.  Temperature
+                  correlates to exact wavelengths of light, so if one value is
+                  known then the other can be found trivially via Wien's
+                  Displacement Law.",
+    stellarage: "The age of a star seems like it might be challenging to calculate,
+                but only two values are needed in order to make a good estimate
+                for a main sequence star: mass and luminosity.  As a star ages,
+                it will become more luminous.",
     stellarmass: "mass info",
     strellarradius: "radius info",
     spectraltype: "spectype info",
