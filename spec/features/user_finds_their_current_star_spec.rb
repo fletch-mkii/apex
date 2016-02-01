f747b954821364ae8a325c683ee3bae7102eb957
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-31 22:20:44 -0500

    added tests for collection population

--- spec/features/user_finds_their_current_star_spec.rb
+++ spec/features/user_finds_their_current_star_spec.rb
@@ -15,4 +15,26 @@ feature "user can find their star" do
     expect(page).to have_content(star.right_ascension)
     expect(page).to have_content(star.declination)
   end
+
+  scenario "star is successfully added to a user's collection" do
+    sign_in_as(user)
+    visit root_path
+    click_on "Find Your Apex"
+    click_on "Profile"
+
+    expect(page).to have_content(user.username)
+    expect(page).to have_content(star.star_name)
+  end
+
+  scenario "user can view past stars from their profile" do
+    sign_in_as(user)
+    visit root_path
+    click_on "Find Your Apex"
+    click_on "Profile"
+    click_on star.star_name
+
+    expect(page).to have_content(star.star_name)
+    expect(page).to have_content(star.right_ascension)
+    expect(page).to have_content(star.declination)
+  end
 end
